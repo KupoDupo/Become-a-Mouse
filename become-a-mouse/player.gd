8bfc263200d13e38e1a5769aadb54ad0b10ae6b1
KupoDupo <lnkleijnen@gmail.com>
2025-11-17 18:19:49 -0800

    Camera and dark shadows implemented

--- become-a-mouse/player.gd
+++ become-a-mouse/player.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
 @export var speed := 150.0
+@onready var darkness_mat = get_tree().get_root().get_node("Maze/CanvasLayer/ColorRect").material
+@onready var camera = get_viewport().get_camera_2d()
 
+func _process(delta):
+	# Player is at center of screen if Camera2D follows
+	var screen_center = get_viewport().get_visible_rect().size / 2
+
+	# Update shader
+	darkness_mat.set_shader_parameter("player_pos", screen_center)
+
+	# Update screen size
+	darkness_mat.set_shader_parameter("screen_size", get_viewport().get_visible_rect().size)
+	
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 
