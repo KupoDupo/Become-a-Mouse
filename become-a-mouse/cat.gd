75718cd4d76d566f1f08be281539b237778f66a2
thuynh26 <thuynh26@ucsc.edu>
2025-12-02 13:39:27 -0800

    (LOISEL'S WORK) : Vision cone rotates with cat

--- become-a-mouse/cat.gd
+++ become-a-mouse/cat.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 @export var player: Node2D
 @export var chase_speed: float = 120.0
 
+@onready var vision_cone = $VisionCone
 @export var attention_gain_in_zone: float = 10.0
 @export var attention_gain_facing_max: float = 20.0
 @export var attention_loss_outside: float = 5.0
@@ -77,8 +78,11 @@ func _physics_process(delta: float) -> void:
 			else:
 				state_time_left -= delta
 				velocity = direction * move_speed
-				move_and_slide()
 
+				if velocity.length() > 0:
+					vision_cone.rotation = velocity.angle()
+				move_and_slide()
+				
 				if state_time_left <= 0.0:
 					_enter_idle_state()
 
@@ -105,14 +109,14 @@ func _enter_walk_state() -> void:
 
 
 func _chase_player(delta: float) -> void:
-	print(player, "IS HERE")
-
 	if player == null or not player_in_light_zone:
 		_enter_idle_state()
 		return
 
 	var to_player: Vector2 = (player.global_position - global_position).normalized()
 	velocity = to_player * chase_speed
+	if velocity.length() > 0:
+		vision_cone.rotation = velocity.angle()
 	move_and_slide()
 
 	if attention <= attention_drop_chase_threshold and not player_in_vision:
