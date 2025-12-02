0d4e19ad9c38be2da5712d51a73759bdf5fed39b
MaxWelburn <maxgreek04@gmail.com>
2025-12-02 14:28:33 -0800

    minor tweaks

--- become-a-mouse/attention_bar.gd
+++ become-a-mouse/attention_bar.gd
@@ -6,7 +6,7 @@ class_name attention_ui
 func set_attention(v: int) -> void:
 	var t: float = clampf(float(v) / 100.0, 0.0, 1.0)
 	var new_size: Vector2 = attention_bar.size
-	new_size.x = 313 * t
+	new_size.x = 219 * t
 	attention_bar.size = new_size
 	var stylebox := attention_bar.get_theme_stylebox("panel") as StyleBoxFlat
 	var green: Color = Color8(0, 255, 0)
